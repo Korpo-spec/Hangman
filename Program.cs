5cc48703edc245c8169b95ef5bf8a692c1000328
Casper <Casper.sandstrom@elev.ga.ntig.se>
2020-09-01 10:53:30 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Välj ett ord:");
+            string word = Console.ReadLine();
+
+            char[] lettersInWord = word.ToCharArray();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.Write("_ ");
+            }
+            
+            System.Console.WriteLine();
+
+            while (true)
+            {
+                
+                
+                
+                if(lettersInWord.Contains(CheckValidInput())){
+
+
+
+                }
+            }
+        }
+        static char CheckValidInput(){
+
+            string letterGuess = "hej";
+            bool isLetter = true;
+            while (letterGuess.Length != 1 ||  isLetter)
+            {
+                Console.WriteLine("Välj en bokstav:");
+                
+                letterGuess = Console.ReadLine();
+                int hej;
+                isLetter = int.TryParse(letterGuess, out hej);
+            }
+
+            return letterGuess.ToCharArray()[0];
+
+        }
+    }
+}
